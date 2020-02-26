3618577a0aec8622657a8465c02b8cfab4d20fd2
AhabweEmmanuel <ahabweemma@gmail.com>
2020-02-26 19:24:30 +0300

    Update rave.charge.js

--- services/rave.charge.js
+++ services/rave.charge.js
@@ -42,7 +42,7 @@ var spec = morx.spec()
 	.build('orderRef', 'required:false')
 	.build('redirect_url', 'required:false,eg:http://your_redirect_url.com')
 	.build('3DS_OVERRIDE', 'required:false')
-	.build('network', 'required:true, eg:UGX')
+	.build('network', 'required:false, eg:UGX')
 	.end();
 
 function service(data, _rave) {
